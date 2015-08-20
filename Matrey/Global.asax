13ee348a636891bc27ff1f5328f06246ae294b66
santhosh.v <santhosh.v@redbus.in>
2015-08-20 05:53:05 +0530

    Project Setup

---
+++ Matrey/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Matrey.MvcApplication" Language="C#" %>
---
+++ Matrey/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Matrey.Startup))]
+namespace Matrey
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
